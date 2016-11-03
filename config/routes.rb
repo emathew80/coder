b6e154f5eec1597d00914e3d341d3b18a1f764f6
Elson Mathew <emathew.dev@gmail.com>
2016-11-03 04:25:15 +0000

    Listing the courses and creating about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'static_pages#index'
+  get 'about', to: 'static_pages#about'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
