9357afa299e9f5b2f59b6b76ef36af24d44716ee
Elson Mathew <emathew.dev@gmail.com>
2016-12-02 20:06:18 +0000

    Add authentication for lessons page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,6 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user! 
+  before_action :check_enrollment
   
   
 
@@ -9,6 +10,9 @@ class LessonsController < ApplicationController
 
 private
 
+def check_enrollment  
+ return render text: 'Unauthorized', status: :unauthorized unless current_user.enrolled_in?(current_lesson.section.course)
+end
 
 helper_method :current_lesson
 def current_lesson
