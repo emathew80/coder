e11c6d085764a5ce78c7b5eb5b1408fc3b045c29
Elson Mathew <emathew.dev@gmail.com>
2016-12-27 15:51:53 +0000

    Add user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resource :dashboard, only: [:show]
+
   root 'static_pages#index'
   get 'about', to: 'static_pages#about'
   resources :courses, only: [:index, :show] do
